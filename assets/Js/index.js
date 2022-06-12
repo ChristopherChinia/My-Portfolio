cecddba9df616156d0c5e7b7d4a71971d01c30fb
christopherchinia <chinia214@gmail.com>
2022-06-12 13:09:10 -0400

    Adding styles for home section class navBar and responsiveness adding files for javascript for the home section

--- assets/Js/index.js
+++ assets/Js/index.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
-    $(window).scroLL(function(){
-        if(this.scroLLY>20){
-
-        }else{
-            
-        }
-    })
-})
+  $(window).scroLL(function(){
+    if(this.scroLLY > 20){
+      $(".navBar").addClass("sticky");
+    }else {
+      $(".navBar").removeClass("sticky");
+    }
+  });
+//   toggle menu/navBar script
+$('.menu-btn').click(function(){
+    $('.navBar .menu').toggleClass("active")
+});
+});
